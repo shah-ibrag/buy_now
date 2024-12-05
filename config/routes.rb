11b1a5bd2cab41708be9264a4a387a74dbff0285
Shawn Ibragimov <shah.ibrag@gmail.com>
2024-12-05 05:14:56 -0600

    admin log in and dashboard created

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
   root "products#index"
+  devise_for :admins
   resources :products, only: [:index, :show] do
     collection do
       get 'search'
     end
   end
   resources :categories, only: [:index, :show]
+  get 'admin_dashboard', to: 'admin_dashboard#index'
 end
